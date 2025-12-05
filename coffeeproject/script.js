18892d7687411d95c9f3fdbd5a8ac25d6c8858a9
TejSurya <tejasuryadenduluri@gmail.com>
2025-12-05 10:14:14 +0530

    Add files via upload

---
+++ coffeeproject/script.js
@@ -0,0 +1,55 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+
+
+const menuOpenButton = document.querySelector("#menu-open-button");
+
+ menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+ });
+const menuCloseButton = document.querySelector("#menu-close-button");
+// close menu when the close button is clicked
+ menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// close menu when the nav link is clicked
+
+navLinks.forEach(link => {
+   link.addEventListener("click", () => menuOpenButton.click());
+ });
+
+
+ 
+ // initialize swiper 
+ const swiper = new Swiper('.slider-wrapper', {
+  
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  //responsive break points
+  breakpoints:{
+   0:{
+      slidesPerView: 1
+   },
+   768:{
+      slidesPerView: 2
+   },
+   1024:{
+      slidesPerView: 3
+   }
+  }
+
+});
+
